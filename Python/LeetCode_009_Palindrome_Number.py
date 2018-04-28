e77c60e162cd26e22259876a6aad4f1a3a5f8cf3
Andrew Nicholls <andrewsnicholls@gmail.com>
2018-04-28 10:52:09 -0400

    Files are split into Python and SQL folders and not by website name

--- Python/LeetCode_009_Palindrome_Number.py
+++ Python/LeetCode_009_Palindrome_Number.py
@@ -1,10 +1,10 @@
 # https://leetcode.com/problems/palindrome-number/description/
 # Determine whether an integer is a palindrome. Do this without extra space.
 # Some hints:
-    # Could negative integers be palindromes? (ie, -1)
-    # If you are thinking of converting the integer to string, note the restriction of using extra space.
-    # You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
-    # There is a more generic way of solving this problem.
+# Could negative integers be palindromes? (ie, -1)
+# If you are thinking of converting the integer to string, note the restriction of using extra space.
+# You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+# There is a more generic way of solving this problem.
 
 class Solution:
     def isPalindrome(self, x):
@@ -12,9 +12,9 @@ class Solution:
             return(False)
         if(x == 0):
             return(True)
-        
+
         n_digits = int(math.log10(x))+1
-    
+
         for n in range(n_digits-1,0,-2):
             if (x // 10**n) == (x % 10):
                 x = x % (10**n) // 10
