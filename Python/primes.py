0b312bc6eb7ce7bcd3e6029c887131f2a03621ae
Andrew Nicholls <andrewsnicholls@gmail.com>
2018-10-29 10:37:05 -0700

    Added Fibonacci file, Updated Primes
    Added my Fibonacci number generator function along with the function isPrime.

--- Python/primes.py
+++ Python/primes.py
@@ -1,4 +1,5 @@
 # File for keeping track of functions I've written related to prime numbers.
+from math import sqrt
 
 def prime_numbers():
     '''
@@ -10,4 +11,12 @@ def prime_numbers():
         if all((num%i != 0 for i in range(3, int(sqrt(num))+1, 2))):
             yield num
         num += 2
-        
+
+def is_prime(x):
+    '''Take in an integer value as input and return True or False if the number is prime or not.'''
+    x = int(x)
+    if (x == 2):
+        return True
+    if (x % 2 == 0 or x <= 1):
+        return False
+    return all((x%i != 0 for i in range(3, int(sqrt(x))+1, 2)))        
