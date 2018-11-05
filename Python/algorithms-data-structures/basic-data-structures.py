4e32339ed7df83a21e0486d6015ef48bf9bc72a8
Andrew Nicholls <andrewsnicholls@gmail.com>
2018-11-04 16:06:01 -0800

    Implemented Queue Class
    First in, first out.

--- Python/algorithms-data-structures/basic-data-structures.py
+++ Python/algorithms-data-structures/basic-data-structures.py
@@ -1,4 +1,5 @@
 class Stack:
+    # We define the top of the stack as the last item in the list.
     def __init__(self):
         self.items = []
     
@@ -14,5 +15,22 @@ class Stack:
     def peek(self):
         return self.items[-1]
     
+    def size(self):
+        return len(self.items)
+
+class Queue:
+    # First in, first out. 
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        self.items.pop()
+
     def size(self):
         return len(self.items)
